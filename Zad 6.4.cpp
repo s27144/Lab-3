58d65e6d6b24a0489716709fcf0b742b03c7ba4f
s27144 <118570870+s27144@users.noreply.github.com>
2022-12-30 11:02:14 +0100

    Update Zad 6.4.cpp

--- Zad 6.4.cpp
+++ Zad 6.4.cpp
@@ -1,23 +1,3 @@
-﻿//#include <iostream>
-//#include <string>
-//using namespace std;
-//string wyraz, wyraz2;
-//int n, rozmiar;
-//
-//int main() {
-//	cout << "Podaj wyraz do konkatenacji: ";
-//	getline(cin, wyraz);
-//	rozmiar = wyraz.length();
-//	//strcpy(wyraz2, wyraz);
-//
-//	cout << "Podaj liczbe n: ";
-//	cin >> n;
-//	for (int i = 0; i < n; i++) {
-//		
-//	}
-//
-//	return 0;
-//}
 #include <iostream>
 #include <string>
 using namespace std;
